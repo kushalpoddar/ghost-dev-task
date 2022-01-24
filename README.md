9ea58abee435e292d0b0a34172ea46e7d0e4db89
Kushal Poddar <kpoddar160200@gmail.com>
2022-01-25 02:37:18 +0530

    Deployment - 1

--- README.md
+++ README.md
@@ -23,3 +23,7 @@ To run the application, perform the following steps-
 - Now open http://localhost:6277 in your browser and enjoy
 
 Note - Backend server runs on port 6277 and frontend server runs on 3000 but static build of react is already added to backend due to which we can check the code on 6277
+
+The entire codebase was written in duration of 5-6 hours. I am submitting after 12 hours due to some emergency work in between. The entire commits are present.
+
+Looking forward to work with you!
--- backend/conn.js
+++ backend/conn.js
@@ -1,10 +1,24 @@
+const config = require('config')
+const db_config = config.get('db')
 const mysql = require("mysql");
 
+let host, user, password, database
+if(config.get("type") == 'D'){
+    host = db_config.get("host_dev")
+    user = db_config.get("user_dev")
+    password = db_config.get("password_dev")
+    database = db_config.get("db_dev")
+}else{
+    host = db_config.get("host_prod")
+    user = db_config.get("user_prod")
+    password = db_config.get("password_prod")
+    database = db_config.get("db_prod")
+}
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "ghost-dev",  
+    host,
+    user,
+    password,
+    database,  
 })
 
 db.connect((err) => {
