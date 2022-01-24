deed6c7e899de1cce095fcbd565359e76191e672
Kushal Poddar <kpoddar160200@gmail.com>
2022-01-25 02:43:20 +0530

    Deployment - 3

--- backend/conn.js
+++ backend/conn.js
@@ -2,7 +2,7 @@ const config = require('config')
 const db_config = config.get('db')
 const mysql = require("mysql");
 
-let host, user, password, database
+let host, user, password, database, port = 3306
 if(config.get("type") == 'D'){
     host = db_config.get("host_dev")
     user = db_config.get("user_dev")
@@ -13,12 +13,14 @@ if(config.get("type") == 'D'){
     user = db_config.get("user_prod")
     password = db_config.get("password_prod")
     database = db_config.get("db_prod")
+    port = db_config.get("port_prod")
 }
 const db = mysql.createConnection({
     host,
     user,
     password,
-    database,  
+    database,
+    port  
 })
 
 db.connect((err) => {
